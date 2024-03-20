9f814ac71091e9bd3950d9948ee163595dc51f01
hattorikotaro <164205419+hattorikotaro@users.noreply.github.com>
2024-03-21 00:01:09 +0700

    Update lib.rs

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("DYKs4tYkUpWXjxtKA6JycLfJGBjSzV94WrT1sVUEsUXF");
 
 #[program]
 pub mod nft_lend_borrow {
